8b309613dc5165f3daa2051bbdcd02ec61309317
Aman Chahar <amanchahar1702@gmail.com>
2025-11-07 14:44:55 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const recordsRouter = require('./routes/records');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(express.json());
 
 const MONGO_URI = process.env.MONGO_URI;
+console.log('MONGO_URI', MONGO_URI);
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=>console.log('Mongo connected'))
   .catch(err=>console.error(err));
+
+const allowedOrigins = process.env.ALLOWED_ORIGIN?.split(",") || [];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use('/api/records', recordsRouter);
 
